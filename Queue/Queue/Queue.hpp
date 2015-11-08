99a901e9afc57f84f3fe1acfc8a67fe456708e6e
emiliev <emil_iliev1994@abv.bg>
2015-11-08 11:46:03 +0200

    added linkedQueue

---
+++ Queue/Queue/Queue.hpp
@@ -0,0 +1,14 @@
+//
+//  Queue.hpp
+//  Queue
+//
+//  Created by Emil Iliev on 11/8/15.
+//  Copyright © 2015 Emil Iliev. All rights reserved.
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include <stdio.h>
+
+#endif /* Queue_hpp */
