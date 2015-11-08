ddb1ee781ceb5a0d31a02e6e1506d542fd79f4b8
emiliev <emil_iliev1994@abv.bg>
2015-11-08 11:48:03 +0200

    Modified

--- Queue/Queue/Queue.hpp
+++ Queue/Queue/Queue.hpp
@@ -11,4 +11,71 @@
 
 #include <stdio.h>
 
+template <typename T>
+class Node{
+    
+    public:
+    Node<T>* next;
+    T data;
+    Node(T, Node<T>* = 0);
+};
+
+template <typename T>
+class LinkedQueue{
+    
+    public:
+   
+        LinkedQueue();
+        ~LinkedQueue();
+        void enqueue(T element);
+        void dequeue(T& element);
+        bool isEmpty();
+        T peek();
+
+    private:
+    
+        Node<T>* front;
+        Node<T>* rare;
+        size_t used;
+};
+
+
+
+template <typename T>
+Node<T>::Node(T newData, Box<T>* p = 0){
+    this->data = newData;
+    this->next = p;
+}
+
+template <typename T>
+LinkedQueue<T>::LinkedQueue(){
+    
+    this->used = 0;
+    this-front = 0;
+    this->rare = 0;
+}
+
+template <typename T>
+void LinkedQueue<T>::enqueue(T element){
+    
+    Node<T>* elem = new Node(element,rare);
+    front = elem;
+    used++;
+}
+
+
+bool LinkedQueue<T>::isEmpty{
+    
+    if(this->used > 0){
+        return false;
+    }
+    
+    return true;
+}
+
+void LinkedQueue<T>::dequeue(T& element){
+    
+    
+}
+
 #endif /* Queue_hpp */
