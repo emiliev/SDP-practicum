0a491ecf832a32052914d31d8b492be3a1ac00cb
emiliev <emil_iliev1994@abv.bg>
2015-11-17 23:59:12 +0200

    added description in readME & started HW2

---
+++ HW2/HW2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW2
+//
+//  Created by Emil Iliev on 11/17/15.
+//  Copyright © 2015 Emil Iliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
