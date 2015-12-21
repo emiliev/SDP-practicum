d48738318f7d4feeaccd38cad24955ff71b5dd56
emiliev <emil_iliev1994@abv.bg>
2015-12-21 18:50:26 +0200

    Initial commit

---
+++ LinkedList/LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Emil Iliev on 12/21/15.
+//  Copyright © 2015 Emil Iliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
