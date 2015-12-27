ba1de9dfe60c024b3dd143eaf4bfefdceb8559b3
emiliev <emil_iliev1994@abv.bg>
2015-12-27 11:34:20 +0200

    few functions that will help in future

--- MemoryManager/MemoryManager/main.cpp
+++ MemoryManager/MemoryManager/main.cpp
@@ -7,9 +7,44 @@
 //
 
 #include <iostream>
+#include "cstring"
 
-int main(int argc, const char * argv[]) {
+using namespace std;
+
+int main(){
+
+    char buffer[15];
+//    int *number1 = (int*)(&buffer);
+//    *number1 = 123456789;
+    int number = 123456789;
+    memcpy(buffer, &number, sizeof(number));
+    cout<<buffer<<endl;
+    
+    char buffer2[5];
+    int* number1 = (int*)(&buffer2);
+    *number1 = 123456789;
     
     
-    return 0;
+    //posledniq bit na chisloto
+    int result = *number1 & 1;
+    cout<<result;
+    cout<<endl<<(int*)&buffer2<<endl;
+    
+    
+    //invertirane na purviq bit
+    int b  = 19;
+    int result1 = (b ^ 1) & 1;
+    cout<<result1<<endl;
+   
+//    
+//    
+//    cout<<endl<<strlen(buffer)<<" "<<buffer<<endl;
+//    
+//    char buffer1[5];
+//    int number = 123456789;
+//    sprintf(buffer1, "%d", number);
+//    
+//    cout<<strlen(buffer1)<<" "<<buffer1<<endl;
+    
+    
 }
