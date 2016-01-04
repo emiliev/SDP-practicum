e97b4131c3b2c9c8a33ef0440b81613ddf97b216
emiliev <emil_iliev1994@abv.bg>
2016-01-04 23:00:24 +0200

    Hw4 started

---
+++ Hw4/Hw4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hw4
+//
+//  Created by Emil Iliev on 1/4/16.
+//  Copyright © 2016 Emil Iliev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+ 
+    
+    return 0;
+}
--- Key-Value-Store/Key-Value-Store/main.cpp
+++ Key-Value-Store/Key-Value-Store/main.cpp
@@ -17,99 +17,15 @@ using namespace std;
 
 const int kKeyLength = 256;
 
-template <typename T>
-void test(){
-    
-    
-    char names[][10] = {"ala", "bala", "porto", "kalaaa", "kashvala", "salamaa", "guz"};
-    ofstream i1("example1.dat", ios::binary);
-    for(int index = 0; index < 7; ++index){
-        
-        int length = strlen(names[index]);
-        i1.write((char*)&length, sizeof(length));
-        i1.write(names[index], strlen(names[index]));
-    }
-    i1.close();
-    
-    ifstream input("example1.dat", ios::binary);
-    if(input){
-        
-        while (!input.eof()) {
-            
-            int wordLength = 0;
-            input.read((char*)&wordLength, sizeof(wordLength));
-            if(!input){
-                cout<<"krai na inputa \n";
-                break;
-            }
-            char* word = new char[wordLength];
-            input.read(word,wordLength);
-            
-            if(strcmp(word, "guz") == 0){
-                streampos p;
-                p = input.tellg();
-                int remaining = wordLength + sizeof(wordLength);
-                p -= remaining;
-                
-                ofstream newRecord("example1.dat", ios::binary | ios::in);
-                newRecord.seekp(p);
-                if(newRecord){
-                    
-                    while (!input.eof()) {
-                        
-                        int newWordLength = 0;
-                        input.read((char*)&newWordLength, sizeof(newWordLength));
-                        if(!input){
-                            cout<<"krai na newRecord!\n";
-                            break;
-                        }
-                        char* newWord = new char[newWordLength];
-                        input.read(newWord, newWordLength);
-                        newRecord.write((char*)&newWordLength, sizeof(newWordLength));
-                        newRecord.write(newWord, newWordLength);
-                        
-                        cout<<newWordLength<<" "<<newWord<<endl;
-                        delete newWord;
-                    }
-                    
-                    while (remaining != 0) {
-                        
-                        newRecord<<'\0';
-                        remaining--;
-                    }
-                }
-                
-                newRecord.close();
-            }
-            cout<<wordLength<<" "<<word<<endl;
-            delete word;
-        }
-    }
 
-    input.close();
-    
-    
-//    ifstream testReader("example1.dat", ios::binary);
-//    if(testReader){
-//        
-//        while (!testReader.eof()) {
-//            
-//            int num = 0;
-//            testReader.read((char*)&num, sizeof(num));
-//            if(!testReader){
-//                
-//                cout<<"Krai na cheteneto!\n";
-//                break;
-//            }
-//            
-//            char* bufferNum = new char[num];
-//            testReader.read(bufferNum, num);
-//            cout<<num<<" "<<bufferNum<<endl;
-//            
-//            delete bufferNum;
-//        }
-//    }
-}
+/////////TODO/////////
+/*
+
+ UNIQUE KEYS
+ REWORED SEARCH FUNCTION IN LINKED LIST
+ 
+ ETC..
+*/
 
 int main(int argc, const char * argv[]) {
     
@@ -128,8 +44,9 @@ int main(int argc, const char * argv[]) {
         cout<<"found it for: "<<(clock() - begin_time) / CLOCKS_PER_SEC<<endl;
         cout<<temp.key<<" for value: "<<temp.value<<endl;
     }
+    else{
+        cout<<"found nothing \n";
+    }
 
-    
-//    test<char*>();
     return 0;
 }
